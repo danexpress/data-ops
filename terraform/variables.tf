8022514d37956cffe9c2a02a84e81ed5e1123514
danexpress <fredduoduoffei@gmail.com>
2026-03-17 03:16:09 -0700

    initializing terraform2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,20 @@ variable "aws_region" {
   type        = string
   default     = "us-west-2"
 }
+
+variable "project_name" {
+  default= "dml-temperature-analysis"
+  description = "The project name"
+}
+
+variable "use_existing_bucket" {
+  description = "set to true to use an existing S3 bucket instead of creating a new one"
+  type        = bool
+  default     = false
+}
+
+variable "existing_bucket_name" {
+  description = "The name of the existing S3 bucket"
+  type        = string
+  default     = "dml-temperature-analysis-raw-data"
+}
